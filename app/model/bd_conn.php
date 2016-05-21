5659922e3c1762da3be34f3914c6845a8855f7cd
guillevalen <guivalen@gmail.com>
2016-05-20 23:04:03 -0500

    Conexion exitosa

--- app/model/bd_conn.php
+++ app/model/bd_conn.php
@@ -1,16 +1,17 @@
 <?php
   class ConexionBD{
-	private static $bdhost = "173.45.161.113";
+	private static $bdhost = "dnjs2.wnkserver2.com:3306";
 	private static $bdname = "bsstudio_tecnocuero";
 	private static $bduser = "bsstudio_root";
 	private static $bdpass = "kSQiNwOc5Jz4n7TEMy";
 	private static $conn = null;
+
 	public static function AbrirBD(){
 	 	if(self::$conn == null){
 			 try{
 			 	self::$conn = new PDO('mysql:host='.self::$bdhost.';dbname='.self::$bdname.'',self::$bduser, self::$bdpass);
 			 	self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "conecto";
+       
 			 }catch(PDOException $e){
 
 			 	echo $e->getMessage();
